6579d5f22470fb2d7da950a0a747752169a92b12
dungates <duncan.gates123@gmail.com>
2022-03-11 13:16:49 -0600

    add license and remove unnecessary dashboard functions

--- R/survey_monkey.R
+++ R/survey_monkey.R
@@ -1,6 +1,6 @@
 #' A survey monkey text selection
 #'
-#' Survey monkey reviews from the course survey in response to the question 
+#' Survey monkey reviews from the course survey in response to the question
 #' What is the learning from this course that you are most excited about trying out?, as a data frame.
 #'
 #' @format A data frame with 87 rows and 1 variable:
