9df989b976333de70350f7ec8d48cd92722944b4
dungates <duncan.gates123@gmail.com>
2024-09-30 23:25:45 -0700

    hopefully it builds now

--- R/square.R
+++ R/square.R
@@ -5,8 +5,8 @@
 #' @param x A numeric input.
 #' @return The square of the input.
 #' @examples
-#' square(2)
-#' square(3.5)
+#' tlShiny::square(2)
+#' tlShiny::square(3.5)
 square <- function(x) {
   return(x^2)
 }
