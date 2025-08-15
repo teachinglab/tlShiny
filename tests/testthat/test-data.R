4ddff0f4b38b9169c025eaade3f7ae3165b0e30e
dungates <duncan.gates123@gmail.com>
2025-08-14 22:52:52 -0700

    update subsites

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -4,11 +4,12 @@ test_that("Check that subsites are up to date", {
   subsite_list <- qualtRics::survey_questions("SV_5bBw9H3DUZeBuTA") |>
     dplyr::select(qname)
   subsites_check <- subsite_list |>
-    dplyr::slice(6:43) |>
+    dplyr::slice(6:42) |>
     dplyr::pull()
 
   current_subsites <- c("ar_osceola",
                         "ca_santa_ana",
+                        "ct_hamden",
                         "il_cps",
                         "district6",
                         "district6_other",
@@ -30,27 +31,26 @@ test_that("Check that subsites are up to date", {
                         "district75_other",
                         "ma_boston",
                         "ma_dennis_yarm",
+                        "ma_greenfield",
                         "ma_milford",
                         "ma_plymouth",
                         "ma_uxbridge",
                         "ma_westspring",
-                        "ms_kemper",
+                        "mo_kipp",
                         "nj_great_oaks",
                         "ny_ascend",
                         "ny_cuny",
-                        "ny_lion",
                         "ny_transfer",
                         "tn_acceleration",
                         "tn_acceleration2",
-                        "tx_elpaso",
-                        "tx_la_joya",
-                        "wi_milwaukee")
+                        "tx_la_joya")
 
   expect_true(length(intersect(subsites_check, current_subsites)) == length(current_subsites))
   # setdiff(subsites_check, current_subsites)
   # subsites <- current_subsites |>
   #   setNames(c("AR_Osceola School District",
   #              "CA_Santa Ana Unified School District",
+  #              "CT_Hamden",
   #              "IL_CPS",
   #              "NY_D6", "NY_D6 Other",
   #              "NY_D9", "NY_D9 Other",
@@ -63,21 +63,19 @@ test_that("Check that subsites are up to date", {
   #              "NY_D75", "NY_D75 Other",
   #              "MA_Boston",
   #              "MA_Dennis-Yarmouth",
+  #              "MA_Greenfield",
   #              "MA_Milford",
   #              "MA_Plymouth",
   #              "MA_Uxbridge",
   #              "MA_West Springfield",
-  #              "MS_Kemper County School District",
+  #              "MO_KIPP Kansas City",
   #              "NJ_Great Oaks Legacy Charter Schools",
   #              "NY_Ascend Charter Schools",
   #              "NY_CUNY/UA",
-  #              "NY_LION Charter Schools",
   #              "NY_Transfer High Schools",
   #              "TN_Acceleration for All",
   #              "TN_Acceleration for All",
-  #              "TX_El Paso Leadership Academy",
-  #              "TX_La Joya ISD",
-  #              "WI_Milwaukee Public Schools"))
+  #              "TX_La Joya ISD"))
   # usethis::use_data(subsites, overwrite = TRUE)
 })
 
